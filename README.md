c7caf1f4d0b3d812079db58a8aac8d6ed80a1b41
gui <guizits200@gmail.com>
2025-05-24 10:45:39 -0300

    clone lazy.vim and add first plugin

---
+++ README.md
@@ -0,0 +1,2 @@
+# NVIM-DC-IDE
+The highlight and customizable neovim for development
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+    require("rose-pine").setup({
+      variant = "auto", -- auto, main, moon, or dawn
+      dark_variant = "main", -- main, moon, or dawn
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+
+      enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true, -- Handle deprecated options automatically
+      },
+
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
+    })
+		vim.cmd("colorscheme rose-pine")
+	end
+}
