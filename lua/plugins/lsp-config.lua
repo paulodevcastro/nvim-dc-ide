dfb628667170943f1b017dc880d7e897ec98a022
gui <guizits200@gmail.com>
2025-05-27 11:36:47 -0300

    config javascript for eslint_d

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,11 +1,11 @@
 return {
-  {
+	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
 		end,
 	},
-  {
+	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
@@ -13,30 +13,46 @@ return {
 			})
 		end,
 	},
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local util = require("lspconfig.util")
 
-      -- Config LSPs
-      lspconfig.lua_ls.setup({ capabilities = capabilities, })
-      lspconfig.ts_ls.setup({ capabilities = capabilities, })
-      lspconfig.cssls.setup({ capabilities = capabilities, })
-      lspconfig.emmet_ls.setup({ capabilities = capabilities, })
+			-- Config LSPs
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			-- High config for ts_ls and js
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				root_dir = function(fname)
+					return util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git")(fname)
+						or util.path.dirname(fname)
+				end,
+				settings = {
+					javascript = {
+						format = { enable = true },
+					},
+					typescript = {
+						format = { enable = true },
+					},
+				},
+			})
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.emmet_ls.setup({ capabilities = capabilities })
 
-      -- Keyboard Maps
-      vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+			-- Keyboard Maps
+			vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
 			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
 
-      vim.keymap.set(
+			vim.keymap.set(
 				"n",
 				"<leader>cr",
 				require("telescope.builtin").lsp_references,
 				{ desc = "[C]ode Goto [R]eferences" }
 			)
-      vim.keymap.set(
+			vim.keymap.set(
 				"n",
 				"<leader>ci",
 				require("telescope.builtin").lsp_implementations,
@@ -44,6 +60,36 @@ return {
 			)
 			vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
 			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-    end
-  }
+
+			-- Show diagnostics display automatically
+			-- Reduce the time to trigger the CursorHold
+			vim.o.updatetime = 250
+
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+					spacing = 2,
+					severity = vim.diagnostic.severity.ERROR,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+
+			-- Open diagnostic automatically in floating window
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					vim.diagnostic.open_float(nil, {
+						focusable = false,
+						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+						border = "rounded",
+						source = "always",
+						prefix = "",
+						scope = "cursor",
+					})
+				end,
+			})
+		end,
+	},
 }
